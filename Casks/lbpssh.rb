755b4aa8e03302a56a2c5c31be7ac0a368102d5b
liuboping <liuboping@elex-tech.com>
2026-02-07 16:42:29 +0800

    Initial cask for lbpSSH

---
+++ Casks/lbpssh.rb
@@ -0,0 +1,16 @@
+cask "lbpssh" do
+  version "1.0.1"
+  sha256 :no_check
+
+  url "https://github.com/lbp008/lbpssh/releases/download/v#{version}/lbpSSH-macos-universal.zip"
+  name "lbpSSH"
+  desc "Cross-platform SSH terminal manager"
+  homepage "https://github.com/lbp008/lbpssh"
+
+  app "lbpSSH.app"
+
+  zap trash: [
+    "~/Library/Application Support/lbpSSH",
+    "~/Library/Preferences/com.lbpssh.app.plist",
+  ]
+end
