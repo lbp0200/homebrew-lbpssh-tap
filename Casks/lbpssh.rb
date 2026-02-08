7cf5a3049cd19688434f3a49f7d9737be14122da
liuboping <liuboping@elex-tech.com>
2026-02-09 05:37:08 +0800

    1

--- Casks/lbpssh.rb
+++ Casks/lbpssh.rb
@@ -2,7 +2,7 @@ cask "lbpssh" do
   version "v1.0.0"
   sha256 ""
 
-  url "https://github.com/lbp0200/lbpssh/releases/download/v#{version}/lbpSSH-macos-universal.zip"
+  url "https://github.com/lbp0200/lbpssh/releases/download/#{version}/lbpSSH-macos-universal.zip"
   name "lbpSSH"
   desc "Cross-platform SSH terminal manager"
   homepage "https://github.com/lbp0200/lbpssh"
